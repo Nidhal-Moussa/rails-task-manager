ee5bd25fe2b77196463a125114a706f6f8626314
Nidhal Moussa <sinidhal2001@gmail.com>
2022-09-05 16:59:58 +0200

    index show and update tasks done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,26 +1,29 @@
 class TasksController < ApplicationController
-
+  before_action :find_task, only: [:update, :show, :edit]
   def index
     @tasks = Task.all
   end
 
   def show
-    @tasks = Task.find(params[:id])
+    @task = Task.find(params[:id])
   end
 
-  def another
-    @task = Task.new
+  def edit
+    @task = Task.find(params[:id])
   end
 
-  def create
-    @task = Task.new(params[:task])
-    @task.save
-    redirect_to task_path(@task)
+  def update
+    @task.update(task_params)
+    redirect_to tasks_path
   end
 
   private
 
+  def find_task
+    @task = Task.find(params[:id])
+  end
+
   def task_params
-    params.require(:task).permit(:title, :details)
+    params.require(:task).permit(:title, :details ,:completed)
   end
 end
